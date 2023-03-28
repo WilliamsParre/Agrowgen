6cd1ea8fd5ffcd3d8744af2b4fc56e59e589b670
WilliamsParre <89301149+WilliamsParre@users.noreply.github.com>
2023-03-28 14:21:07 +0530

    Update settings.py

--- Agrowgen/settings.py
+++ Agrowgen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2+t(%dt#n5(v41@&ukz7o-5byf#lu$cty31cwiz9^*msnn+ogu'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
